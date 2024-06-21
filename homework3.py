81f88239bbb97712e149db98729cc48ea357d5a4
Nikitus200 <sm.nikita2007@mail.ru>
2024-06-21 21:59:03 +0500

    Добавил модуль 1 в git

---
+++ homework3.py
@@ -0,0 +1,9 @@
+name = "Nikita"
+print('Name:', name)
+age = 16
+print('Age:', age)
+age = age + 3
+print('New Age:', age)
+is_student = True
+print('Is Student:', is_student)
+# Прошлый файл неправильный
---
+++ main1.py
@@ -0,0 +1,27 @@
+#1)
+a = "Hello, world!"
+print(len(a))
+#2)
+first = 15
+second = 10
+summa = first + second
+diff = first - second
+print(summa, diff)
+#3)
+first = 10
+second = 20
+third = 30
+mean = (first + second + third) / 3
+print(mean)
+#4)
+first_string = "Вторник"
+second_string = "Понедельник"
+print(second_string + ", " + first_string)
+#5)
+a = 2
+b = 3
+c = 4
+f = (a * b) + (a * c) #14
+print(f**3 / 2)
+
+
