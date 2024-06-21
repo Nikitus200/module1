d6ade89dcf3dd3f0b4aaeaa122f78eae4d5926cf
Nikitus200 <sm.nikita2007@mail.ru>
2024-06-21 22:18:44 +0500

    Изменил файл HW1

---
+++ HW2.py
@@ -0,0 +1,7 @@
+Number_of_homework = 12
+hours = 1.5
+Course = 'Python'
+time = hours / Number_of_homework
+hours = str(hours)
+Number_of_homework = str(Number_of_homework)
+print("Курс:", Course + ", всего задач:", Number_of_homework + ", затрачено часов:", hours + ", среднее время выполнения", time, "часа.")
